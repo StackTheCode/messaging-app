5a1a9c9a9adafcf42edb1ceff7bcd72271e4b796
Your Name <you@example.com>
2025-08-09 11:15:04 +0500

    searchbar for users and registration

--- src/main/java/com/rkchat/demo/controllers/UserController.java
+++ src/main/java/com/rkchat/demo/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.rkchat.demo.User;
 import com.rkchat.demo.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,4 +21,12 @@ public class UserController {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+    @GetMapping("/search")
+    public  List<User> searchUsers(@RequestParam(required = false) String query) {
+        if ( query !=null && !query.trim().isEmpty()) {
+            return  userRepository.findByUsernameContainingIgnoreCase(query);
+        }
+        return userRepository.findAll();
+    }
+
 }
--- src/main/java/com/rkchat/demo/repositories/UserRepository.java
+++ src/main/java/com/rkchat/demo/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.rkchat.demo.repositories;
 import com.rkchat.demo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
    Optional <User> findByUsername(String username);
+   List<User> findByUsernameContainingIgnoreCase(String username);
 }
